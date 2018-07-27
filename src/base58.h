9f92b85c42276b415a158e9a96d9b8451716b0bd
hanebuechenes <hanebuechenes@gmail.com>
2018-07-28 00:22:03 +0200

    address 2

--- src/base58.h
+++ src/base58.h
@@ -278,7 +278,7 @@ public:
     enum
     {
         PUBKEY_ADDRESS = 38,
-        SCRIPT_ADDRESS = 38,
+        SCRIPT_ADDRESS = 39,
         PUBKEY_ADDRESS_TEST = 111,
         SCRIPT_ADDRESS_TEST = 196,
     };
